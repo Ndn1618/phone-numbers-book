02a15dabe9e6ef57cf7b8b89d26848586486ec89
Ndn1618 <ndilbar1618@gmail.com>
2020-11-14 05:17:08 +0500

    Add show phone number function

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,54 @@
-// ;(
-//   async function () {
+// Choosing elements
+var elPhonebookForm = $('.js-phonebook-form');
+var elNameInput = $('.js-name-input', elPhonebookForm);
+var elSurnameInput = $('.js-surname-input', elPhonebookForm);
+var elPhoneInput = $('.js-phone-input', elPhonebookForm);
+var elRelateInput = $('.js-relate-input', elPhonebookForm);
 
-//     const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-
-//     const data = await response.json()
-
-//     for (let u of data) {
-
-//       const newLi = document.createElement('LI')
-//       newLi.textContent = '"userId": ' + u.userId
-//       users.appendChild(newLi)
-
-//       if (u.completed == true) {
-//         newLi.style.backgroundColor = '#a6dcfb'
-//         newLi.style.borderColor = '#52b9f5'
-//       }
-//       else {
-//         newLi.style.backgroundColor = '#a8fdf5'
-//         newLi.style.borderColor = 'turquoise'
-//       }
-
-//       const newDiv = document.createElement('DIV')
-//       newLi.appendChild(newDiv)
-
-//       const idElement = document.createElement('P')
-//       idElement.textContent = '"id": ' + u.id
-//       newDiv.appendChild(idElement)
-
-//       const titleElement = document.createElement('P')
-//       titleElement.textContent = '"title": ' + u.title
-//       newDiv.appendChild(titleElement)
-
-//       const completedElement = document.createElement('P')
-//       completedElement.textContent = '"completed": ' + u.completed
-//       newDiv.appendChild(completedElement)
-//     }
-//   }
-//   )()
-
-var newPhoneNumber = {};
+var elPhoneNumberList = $('.phone-number-list');
 
 var phoneNumbers = [];
+var newPhoneNumber = {};
+
+// Function for showing new phone number
+var showNewPhoneNumber = function () {
+    var elPhoneNumberItem = document.createElement('li');
+    elPhoneNumberItem.classList.add('list-group-item');
+
+    var elPhoneNumberParagraphName = document.createElement('p');
+    elPhoneNumberParagraphName.textContent = 'Name : ' + newPhoneNumber.name;
+
+    var elPhoneNumberParagraphSurname = document.createElement('p');
+    elPhoneNumberParagraphSurname.textContent = 'Surname : ' + newPhoneNumber.surname;
+
+    var elPhoneNumberParagraphPhone = document.createElement('p');
+    elPhoneNumberParagraphPhone.textContent = 'Phone number : ' + newPhoneNumber.phone;
+
+    var elPhoneNumberParagraphRelate = document.createElement('p');
+    elPhoneNumberParagraphRelate.classList.add('mb-0');
+    elPhoneNumberParagraphRelate.textContent = 'Relativeness : ' + newPhoneNumber.relativeness;
+
+    elPhoneNumberList.appendChild(elPhoneNumberItem);
+    elPhoneNumberItem.appendChild(elPhoneNumberParagraphName);
+    elPhoneNumberItem.appendChild(elPhoneNumberParagraphSurname);
+    elPhoneNumberItem.appendChild(elPhoneNumberParagraphPhone);
+    elPhoneNumberItem.appendChild(elPhoneNumberParagraphRelate);
+}
+
+elPhonebookForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+
+  // Taking value of name, surname, phone number and relativeness inputs
+  var elNameInputValue = elNameInput.value.trim();
+  var elSurnameInputValue = elSurnameInput.value.trim();
+  var elPhoneInputValue = parseInt(elPhoneInput.value.trim(), 10);
+  var elRelateInputValue = elRelateInput.value.trim();
+
+  newPhoneNumber.name = elNameInputValue;
+  newPhoneNumber.surname = elSurnameInputValue;
+  newPhoneNumber.phone = elPhoneInputValue;
+  newPhoneNumber.relativeness = elRelateInputValue;
+  phoneNumbers.push(newPhoneNumber);
+
+  showNewPhoneNumber();
+});
